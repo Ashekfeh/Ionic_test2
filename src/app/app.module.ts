6105dc1699125b451e00b1493bda866dd4a1a63c
Abd-Allah <abd.shekfeh@Gmail.com>
2017-08-14 17:50:24 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,21 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { PostPage } from '../pages/post/post';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Data } from '../providers/data/data';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule,AngularFireDatabase,FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth'
+const config = {
+    apiKey: "AIzaSyDjQ82CvBlivhtCLx81mrVyHHOmn5LzkwE",
+    authDomain: "prject-a9421.firebaseapp.com",
+    databaseURL: "https://prject-a9421.firebaseio.com",
+    projectId: "prject-a9421",
+    storageBucket: "prject-a9421.appspot.com",
+    messagingSenderId: "696161834090"
+}
 
 @NgModule({
   declarations: [
@@ -17,11 +29,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    PostPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFireAuth
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +45,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    PostPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Data
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+
 import { NavController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  posts: FirebaseListObservable<any>
+  constructor(public navCtrl: NavController, public af: AngularFire) {
+    this.posts = af.database.list('/posts')
   }
 
 }
